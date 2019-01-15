ed5a26c5ccc77931e3e6b2a2f8724f711f8e411c
stCarolas <stcarolas@gmail.com>
2019-01-15 15:39:25 +0300

    fix dir

--- deployment/publish.sh
+++ deployment/publish.sh
@@ -2,6 +2,6 @@ cd configs
 docker login -u $DOCKERHUB_LOGIN -p $DOCKERHUB_PASSWORD
 for file in *
 do
-  docker build . -t stcarolas/gocd-agent-$file:latest --build-arg AGENT_TYPE=$file ..
+  docker build -t stcarolas/gocd-agent-$file:latest --build-arg AGENT_TYPE=$file ../
   docker push stcarolas/gocd-agent-$file:latest
 done
